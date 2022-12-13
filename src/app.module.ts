6ed1cb161816abeef8da7db1f28c16c9d22a80cf
GakutoOkawara <ookawara0817@gmail.com>
2022-12-14 00:52:21 +0900

    add book module and book model

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), "src/schema.gql"),
       playground: true
-    })
+    }),
+    BooksModule
   ],
   controllers: [AppController],
   providers: [AppService],
